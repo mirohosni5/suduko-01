54cf4e530991a3f912ef6977d298a6d6d41c48b1
mayar <mirohosni5@gmail.com>
2025-12-20 00:01:20 +0200

    auto: MAYAR 2025-12-20T00:01:20.0790772+02:00

--- src/SudokuSolutionVerifier/UndoManager.java
+++ src/SudokuSolutionVerifier/UndoManager.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class UndoManager {
 
     private Stack<UserAction> actions = new Stack<>();
-
+//this is called 
     public void addAction(int row, int col, int oldValue) {
         actions.push(new UserAction(row, col, oldValue));
     }
