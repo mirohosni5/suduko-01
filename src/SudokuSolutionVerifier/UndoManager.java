fb559c58a4f566fa026d6a4cc3a77f6470df8ffe
mayar <mirohosni5@gmail.com>
2025-12-19 23:52:04 +0200

    auto: MAYAR 2025-12-19T23:52:04.8968984+02:00

--- src/SudokuSolutionVerifier/UndoManager.java
+++ src/SudokuSolutionVerifier/UndoManager.java
@@ -1,4 +1,19 @@
 package SudokuSolutionVerifier;
 
+import java.util.Stack;
+
 public class UndoManager {
+
+    private Stack<UserAction> actions = new Stack<>();
+
+    public void addAction(int row, int col, int oldValue) {
+        actions.push(new UserAction(row, col, oldValue));
+    }
+
+    public UserAction undo() {
+        if (actions.empty()) {
+            return null;
+        }
+        return actions.pop();
+    }
 }
