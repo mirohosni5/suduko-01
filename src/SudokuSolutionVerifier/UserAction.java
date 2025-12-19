7a3609b65bb03cadcb18e996a4d78a2106787049
mayar <mirohosni5@gmail.com>
2025-12-19 23:51:20 +0200

    auto: MAYAR 2025-12-19T23:51:20.7719512+02:00

--- src/SudokuSolutionVerifier/UserAction.java
+++ src/SudokuSolutionVerifier/UserAction.java
@@ -1,4 +1,13 @@
 package SudokuSolutionVerifier;
 
 public class UserAction {
+    public int row;
+    public int col;
+    public int oldValue;
+
+    public UserAction(int r, int c, int old) {
+        row = r;
+        col = c;
+        oldValue = old;
+    }
 }
