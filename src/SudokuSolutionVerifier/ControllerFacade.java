295b6f4b12c6caf9625c44865091328b29ccd82a
mayar <mirohosni5@gmail.com>
2025-12-19 23:53:05 +0200

    auto: MAYAR 2025-12-19T23:53:05.0976197+02:00

--- src/SudokuSolutionVerifier/ControllerFacade.java
+++ src/SudokuSolutionVerifier/ControllerFacade.java
@@ -1,4 +1,39 @@
 package SudokuSolutionVerifier;
 
 public class ControllerFacade {
+
+    private int[][] board;
+    private UndoManager undoManager;
+
+    public ControllerFacade(int[][] board) {
+        this.board = board;
+        undoManager = new UndoManager();
+    }
+
+    // called when user edits a cell
+    public void changeCell(int row, int col, int value) {
+
+        int oldValue = board[row][col];
+
+        // save for undo
+        undoManager.addAction(row, col, oldValue);
+
+        // write to log file
+        ActionLogger.log(row, col, oldValue, value);
+
+        // update board
+        board[row][col] = value;
+    }
+
+    // undo last move
+    public void undo() {
+        UserAction action = undoManager.undo();
+        if (action != null) {
+            board[action.row][action.col] = action.oldValue;
+        }
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
 }
