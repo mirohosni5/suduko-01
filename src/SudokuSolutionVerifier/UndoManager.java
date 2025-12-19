21f25a27251012bf8af2bac8d403f17ee84469c2
mayar <mirohosni5@gmail.com>
2025-12-20 00:05:14 +0200

    auto: MAYAR 2025-12-20T00:05:14.5645470+02:00

--- src/SudokuSolutionVerifier/UndoManager.java
+++ src/SudokuSolutionVerifier/UndoManager.java
@@ -4,8 +4,8 @@ so push when user edits and pop when user deletes*/
 import java.util.*;
 
 public class UndoManager {
-    //this is called everytime the user changes anything
-    private Stack<UserAction> actions = new Stack<>();
+    /
+    private Stack<UserAction> actions = new Stack<>();/
 //and we will save the old value incase we want to restoreit
     public void addAction(int row, int col, int oldValue) {
         actions.push(new UserAction(row, col, oldValue));
