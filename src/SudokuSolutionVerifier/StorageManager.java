9ad582688cab2ccd48f156a340851c572d31ad85
Rana Mohamed <es.Rana.Mohamed2023@alexu.edu.eg>
2025-12-20 00:50:08 +0200

    class Storage Manager

--- src/SudokuSolutionVerifier/StorageManager.java
+++ src/SudokuSolutionVerifier/StorageManager.java
@@ -4,11 +4,27 @@
  */
 package SudokuSolutionVerifier;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 /**
  *
  * @author M
  */
 public class StorageManager {
+    public static void saveBoard (String path, int[][] board) throws FileNotFoundException{
+        File f = new File(path);
+        f.getParentFile().mkdirs();
+        try(PrintWriter p = new PrintWriter(f)){
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    p.print(board[i][j] + " ");
+                }
+                p.println();
+            }
+        }
+    }
     
     
 }
