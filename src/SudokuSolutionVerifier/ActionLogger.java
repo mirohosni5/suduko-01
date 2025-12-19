ceea540afe4f2b66315932a2cb4aac9a0abd97f1
mayar <mirohosni5@gmail.com>
2025-12-19 23:45:45 +0200

    auto: MAYAR 2025-12-19T23:45:45.5303221+02:00

--- src/SudokuSolutionVerifier/ActionLogger.java
+++ src/SudokuSolutionVerifier/ActionLogger.java
@@ -1,4 +1,24 @@
 package SudokuSolutionVerifier;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class ActionLogger {
+
+    private static final String LOG_FILE = "log.txt";
+
+    // Log one user action
+    public static void log(int row, int col, int oldValue, int newValue) {
+        try (PrintWriter out = new PrintWriter(new FileWriter(LOG_FILE, true))) {
+            out.println(
+                    "Row=" + row +
+                            ", Col=" + col +
+                            ", Old=" + oldValue +
+                            ", New=" + newValue
+            );
+        } catch (IOException e) {
+            System.out.println("Failed to write to log file");
+        }
+    }
 }
