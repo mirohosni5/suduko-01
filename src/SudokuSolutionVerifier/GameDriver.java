37546797189188c47ce6cb1e769ec2de8c2c82d7
Rana Mohamed <es.Rana.Mohamed2023@alexu.edu.eg>
2025-12-20 01:40:16 +0200

    Class game driver

--- src/SudokuSolutionVerifier/GameDriver.java
+++ src/SudokuSolutionVerifier/GameDriver.java
@@ -4,10 +4,32 @@
  */
 package SudokuSolutionVerifier;
 
+import java.io.FileNotFoundException;
+
 /**
  *
  * @author M
  */
 public class GameDriver {
+    private static final String ROOT= "games/";
+    public static void generateAll(int[][] board) throws FileNotFoundException{
+        generateLevel(board, 10, "easy");
+        generateLevel(board, 20, "medium");
+        generateLevel(board, 25, "hard");
+    }
+    public static void generateLevel(int[][] source, int numOfCells, String level) throws FileNotFoundException{
+        int[][] board = copyBoard(source);
+         RandomPairs r = new RandomPairs();
+          for (int[] p : r.generateDistinctPairs(numOfCells)) {
+            board[p[0]][p[1]] = 0;
+        }
+          StorageManager.saveBoard(ROOT + level + "/game.txt",board);
+    }
+     public static int[][] copyBoard(int[][] b) {
+        int[][] c = new int[9][9];
+        for (int i = 0; i < 9; i++)
+            c[i] = b[i].clone();
+        return c;
+    }
     
 }
