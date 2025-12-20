e0efe59bb787d381d6965c694b8d7e15d8144cc9
Aya-Gebril <aya2gebril@gmail.com>
2025-12-21 00:32:52 +0200

    VerificationResult Constructor

--- src/SudokuSolutionVerifier/VerificationResult.java
+++ src/SudokuSolutionVerifier/VerificationResult.java
@@ -6,5 +6,11 @@ public class VerificationResult {
     private GameState state;
     private List<Cell> invalidCells;
 
+    public VerificationResult(GameState state, List<Cell> invalidCells) {
+        this.state = state;
+        this.invalidCells = invalidCells;
+    }
+
+
 }
 
