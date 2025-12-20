fc1ad4e31135c05953874b2886bc8a558fe181c3
Aya-Gebril <aya2gebril@gmail.com>
2025-12-21 00:59:55 +0200

    update SudokuVerifier

--- src/SudokuSolutionVerifier/SudokuVerifier.java
+++ src/SudokuSolutionVerifier/SudokuVerifier.java
@@ -1,4 +1,35 @@
 package SudokuSolutionVerifier;
 
+import java.util.*;
+
 public class SudokuVerifier {
+    public VerificationResult verify(int[][] board) {
+        Set<Cell> invalidCells = new HashSet<>();
+        boolean hasZero = false;
+
+        for (int i = 0; i < 9; i++) {
+            boolean[] used = new boolean[10];
+            Cell[] firstPos = new Cell[10];
+
+            for (int j = 0; j < 9; j++) {
+                int val = board[i][j];
+
+                if (val == 0) {
+                    hasZero = true;
+                    continue;
+                }
+
+                if (used[val]) {
+                    invalidCells.add(firstPos[val]);
+                    invalidCells.add(new Cell(i, j));
+                } else {
+                    used[val] = true;
+                    firstPos[val] = new Cell(i, j);
+                }
+            }
+        }
+
+
+    }
+
 }
