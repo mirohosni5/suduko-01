7767f41812b1812be55849f911be66a2157e0471
Aya-Gebril <aya2gebril@gmail.com>
2025-12-21 01:29:34 +0200

    Game attrib. & const.

--- src/SudokuSolutionVerifier/Game.java
+++ src/SudokuSolutionVerifier/Game.java
@@ -1,4 +1,11 @@
 package SudokuSolutionVerifier;
 
 public class Game {
+    private int[][] board;
+    private GameState state;
+
+    public Game(int[][] board) {
+        this.board = board;
+        this.state = GameState.INCOMPLETE; // default
+    }
 }
