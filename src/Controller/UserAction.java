e0d8edd13b8061d4241825909c66db5ff5e444ad
Aya-Gebril <aya2gebril@gmail.com>
2025-12-21 22:08:55 +0200

    Update UserAction

--- src/Controller/UserAction.java
+++ src/Controller/UserAction.java
@@ -1,11 +1,26 @@
 package Controller;
 
 public class UserAction {
-    public int row, col, oldValue;
+    public int row, col, oldValue, newValue;
 
-    public UserAction(int r, int c, int old) {
+    public UserAction(int r, int c, int oldV, int newV) {
         row = r;
         col = c;
-        oldValue = old;
+        oldValue = oldV;
+        newValue = newV;
+    }
+
+    @Override
+    public String toString() {
+        return row + "," + col + "," + oldValue + "," + newValue;
+    }
+
+    public static UserAction fromString(String s) {
+        String[] parts = s.split(",");
+        int r = Integer.parseInt(parts[0]);
+        int c = Integer.parseInt(parts[1]);
+        int oldV = Integer.parseInt(parts[2]);
+        int newV = Integer.parseInt(parts[3]);
+        return new UserAction(r, c, oldV, newV);
     }
 }
