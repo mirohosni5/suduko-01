f5886aa5eed3f19a8752bde22b9f9208c357a614
Aya-Gebril <aya2gebril@gmail.com>
2025-12-21 21:34:40 +0200

    update UserAction

--- src/Controller/UserAction.java
+++ src/Controller/UserAction.java
@@ -1,4 +1,11 @@
 package Controller;
 
 public class UserAction {
+    public int row, col, oldValue;
+
+    public UserAction(int r, int c, int old) {
+        row = r;
+        col = c;
+        oldValue = old;
+    }
 }
