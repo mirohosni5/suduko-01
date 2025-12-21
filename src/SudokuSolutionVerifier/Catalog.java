f7dc197bbd6ef82218d915c20a332119376d48e1
Aya-Gebril <aya2gebril@gmail.com>
2025-12-21 10:59:06 +0200

    Catalog update

--- src/SudokuSolutionVerifier/Catalog.java
+++ src/SudokuSolutionVerifier/Catalog.java
@@ -6,4 +6,9 @@ public class Catalog {
     private boolean current;
     private boolean allModesExist;
 
+    public Catalog(boolean current, boolean allModesExist) {
+        this.current = current;
+        this.allModesExist = allModesExist;
+    }
+
 }
