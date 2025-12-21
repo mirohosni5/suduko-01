092e1965e22899055c3036f2a941b6d4a096074e
Rana Mohamed <es.Rana.Mohamed2023@alexu.edu.eg>
2025-12-21 14:08:23 +0200

    update

--- src/SudokuSolutionVerifier/GameDriver.java
+++ src/SudokuSolutionVerifier/GameDriver.java
@@ -5,12 +5,17 @@
 package SudokuSolutionVerifier;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
 
 /**
  *
  * @author M
  */
 public class GameDriver {
+    private final StorageManager storage = new StorageManager();
+    private final SequentialMode verifier = new SequentialMode();
+    private final RandomPairs randomPairs = new RandomPairs();
     private static final String ROOT= "games/";
     public static void generateAll(int[][] board) throws FileNotFoundException{
         generateLevel(board, 10, "easy");
@@ -24,6 +29,37 @@ public class GameDriver {
             board[p[0]][p[1]] = 0;
         }
           StorageManager.saveBoard(ROOT + level + "/game.txt",board);
+    }
+     public void generateAllVerified(int[][] sourceBoard)throws InvalidGame, IOException {
+        ValidationResult result = verifier.verify(sourceBoard);
+        
+        if (!result.isValid()) {
+            throw new InvalidGame("Source solution has conflicts");
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (sourceBoard[i][j] == 0) 
+                    throw new InvalidGame("Source solution is incomplete");
+            }
+        }
+        int[][] easyBoard = copyBoard(sourceBoard);
+        int[][] mediumBoard = copyBoard(sourceBoard);
+        int[][] hardBoard = copyBoard(sourceBoard);
+        List<int[]> easyPairs = randomPairs.generateDistinctPairs(10);
+        List<int[]> mediumPairs = randomPairs.generateDistinctPairs(20);
+        List<int[]> hardPairs = randomPairs.generateDistinctPairs(25);
+        for (int[] pair : easyPairs) {
+            easyBoard[pair[0]][pair[1]] = 0;
+        }
+        for (int[] pair : mediumPairs) {
+            mediumBoard[pair[0]][pair[1]] = 0;
+        }
+        for (int[] pair : hardPairs) {
+            hardBoard[pair[0]][pair[1]] = 0;
+        }
+        storage.saveGame(DifficultyEnum.EASY, easyBoard);
+        storage.saveGame(DifficultyEnum.MEDIUM, mediumBoard);
+        storage.saveGame(DifficultyEnum.HARD, hardBoard);
     }
      public static int[][] copyBoard(int[][] b) {
         int[][] c = new int[9][9];
