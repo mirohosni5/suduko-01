9fa658c265c2e665885a7530fe196c717ba17bb8
mayar <mirohosni5@gmail.com>
2025-12-21 02:03:11 +0200

    auto: MAYAR 2025-12-21T02:03:11.1239491+02:00

--- src/gui/BoardGenerator.java
+++ src/gui/BoardGenerator.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class BoardGenerator {
 
-    
+
     private static final int[][] SOLVED_BOARD = {
             {5,3,4,6,7,8,9,1,2},
             {6,7,2,1,9,5,3,4,8},
@@ -16,8 +16,7 @@ public class BoardGenerator {
             {2,8,7,4,1,9,6,3,5},
             {3,4,5,2,8,6,1,7,9}
     };
-
-    // removes N cells randomly
+    
     public static int[][] generate(int removedCells) {
         int[][] board = copy(SOLVED_BOARD);
         Random rand = new Random();
