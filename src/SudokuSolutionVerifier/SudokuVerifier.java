c79512ecc1884fb73f42fbd34af004f803876e6f
Aya-Gebril <aya2gebril@gmail.com>
2025-12-21 11:28:11 +0200

    SudokuVerifier update

--- src/SudokuSolutionVerifier/SudokuVerifier.java
+++ src/SudokuSolutionVerifier/SudokuVerifier.java
@@ -31,8 +31,21 @@ public class SudokuVerifier {
             return true;
         }
 
-
-
+        private boolean checkBoxes(int[][] board) {
+            for (int row = 0; row < 9; row += 3) {
+                for (int col = 0; col < 9; col += 3) {
+                    boolean[] seen = new boolean[10];
+                    for (int i = 0; i < 3; i++) {
+                        for (int j = 0; j < 3; j++) {
+                            int value = board[row + i][col + j];
+                            if (seen[value]) return false;
+                            seen[value] = true;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
 
     }
 }
