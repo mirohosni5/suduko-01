3f3b1b998a4a53404bbb393a534472aefb47b87b
Aya-Gebril <aya2gebril@gmail.com>
2025-12-21 21:42:48 +0200

    Update ActionHistory

--- src/Controller/ActionHistory.java
+++ src/Controller/ActionHistory.java
@@ -1,4 +1,12 @@
 package Controller;
 
+import java.util.Stack;
+
 public class ActionHistory {
+    private Stack<UserAction> stack = new Stack<>();
+
+    public void push(UserAction a) {
+        stack.push(a);
+    }
+
 }
