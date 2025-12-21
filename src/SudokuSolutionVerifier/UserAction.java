1eea7ba4394d334789db54b64e55f15f68b935a9
Rana Mohamed <es.Rana.Mohamed2023@alexu.edu.eg>
2025-12-21 14:02:35 +0200

    update

--- src/SudokuSolutionVerifier/UserAction.java
+++ src/SudokuSolutionVerifier/UserAction.java
@@ -10,4 +10,12 @@ public class UserAction {
         col = c;
         oldValue = old;
     }
+    @Override
+    public String toString() {
+        return row + "," + col + "," + oldValue;
+    }
+    public static UserAction fromString(String str) {
+        String[] parts = str.split(",");
+        return new UserAction(Integer.parseInt(parts[0]),Integer.parseInt(parts[1]),Integer.parseInt(parts[2]));
+    }
 }
