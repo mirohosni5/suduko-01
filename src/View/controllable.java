04ad3df9ddbb7a9f48e289e1a81f0f5529c2f86e
Rana Mohamed <es.Rana.Mohamed2023@alexu.edu.eg>
2025-12-21 14:55:05 +0200

    update

---
+++ src/View/controllable.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package View;
+
+/**
+ *
+ * @author M
+ */
+public interface controllable {
+    boolean getCatalog();
+    boolean[][] verifyGame(int[][] game);
+    int[][] getGame(char level) throws Exception;
+    int[][] solveGame(int[][] game) throws Exception;
+    void driveGames(String sourcePath) throws Exception;
+    void logUserAction(UserAction action) throws Exception; 
+}
